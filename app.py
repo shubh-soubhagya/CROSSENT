2023c2d3ff5fa78f5a6995ed50e294820d9058ad
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-07-21 10:12:53 +0530

    Ticket dummy data evaluatio

--- app.py
+++ app.py
@@ -14,16 +14,8 @@ save_emails_to_csv(emails)
 ############# EMAIL AGENT #################
 
 df = pd.read_csv("emails_cleaned.csv")
-
-# ✅ Extract name/email
 df[['src_name', 'src_email']] = df['From'].apply(extract_name_email)
-
-# ✅ Clean body
 df['new_body'] = df['Body'].apply(clean_email_body)
-
-# ✅ Sentiment Classification
 df[['emotion_sentiment', 'fine_grained_sentiment', 'thinking']] = df['new_body'].apply(classify_sentiment)
-
-# ✅ Save the final result
 df.to_csv("emails_cleaned.csv", index=False)
 print(f"✅ Completed. Saved {len(df)} emails to emails_cleaned.csv with sentiment analysis.")
